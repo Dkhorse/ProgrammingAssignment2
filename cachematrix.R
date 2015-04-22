e622b8d26bf4d52b7e20db66a09e26098f3a21c1
Dkhorse <frey.kd@gmail.com>
2015-04-21 22:02:02 -0700

    Update cachematrix.R
    Solution for Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+##These two functions allow for the creation of a matrix
+##such that the inverse is solved once and stored, with
+##all following calls for the inverse returning the stored
+##inverse until the matrix itself changes.
 
-makeCacheMatrix <- function(x = matrix()) {
+##makeCacheMatrix(x=matrix()) returns a list
+##of functions that operate on a matrix, allowing
+##it and its inverse to be set and cached.
 
+makeCacheMatrix <- function(x= matrix()){
+    i<-NULL
+    ##sets the matrix (x) to value y and the
+    ##stored inverse (i) to NULL
+    make <- function(y){
+        x <<- y
+        i <<- NULL
+    }
+    ##returns the matrix (x)
+    retrieve <- function() x
+    ##stores the given inverse (inv) in variable i
+    setInv <- function(inv) i <<- inv
+    ##returns the stored inverse (i)
+    retrieveInv <- function() i
+    list(make=make, retrieve=retrieve, setInv=setInv,
+        retrieveInv=retrieveInv)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##cacheSolve(x,...) retrieves the inverse, then sets
+##it if it is still NULL. Returns the inverse.
+##Assumes the matrix is square and invertible.
+cacheSolve <- function(x, ...){
+    inverse <- x$retrieveInv()
+    ##sets the inverse w/solve() if inverse is NULL
+    if (is.null(inverse)){
+        mtx <- x$retrieve()
+        inverse <- x$setInv(solve(mtx))
+    }
+    ##redundant code to make clear I'm returning inverse
+    inverse
 }
